a1753e4e81a8b6832045067fa36933de3ad51ab8
Evan Rex <evangeorgerex@gmail.com>
2021-05-07 00:02:00 +0200

    update test.cc

--- RobocupWrapper/test.cc
+++ RobocupWrapper/test.cc
@@ -6,13 +6,13 @@
 using namespace std;
 
 worldmodelWrap testWorldModel(){
-    vector<vector<int>> teammatesIN;
-    vector<int> myPositionIN;
-    vector<int> ballIN;
-    int UNumIN;
-    int NUMAGENTSIN;
+    vector<vector<int>> teammatesIN; //coordinates of each player
+    vector<int> myPositionIN; //my coordinate
+    vector<int> ballIN; //ball coordinate
+    int UNumIN; //my player number
+    int NUMAGENTSIN; //number of players
 
-    //TODO populate above vectors
+    //TODO populate above values with test data
 
     worldmodelWrap instance(teammatesIN, myPositionIN, ballIN, UNumIN, NUMAGENTSIN);
 
