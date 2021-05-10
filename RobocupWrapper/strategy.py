5c273b2d58293dbe5396e2e901cccb9c43215d6c
Elias <ejudin@gmail.com>
2021-05-10 13:20:52 +0200

    Updated strategy.py calling structure

--- RobocupWrapper/strategy.py
+++ RobocupWrapper/strategy.py
@@ -1,4 +1,9 @@
 def selectSkill():
     print('Python function selectSkill() called')
-    skill = 0
+    return calculateSkill()
+
+def calculateSkill():
+    print('Calculating next skill...')
+    #calculations here
+    skill = 2
     return skill
