e762aaf904c41fccfef207b75f6d7c0a2d205fb9
Ntsika Mabe <48208934+ntsikaniceguy@users.noreply.github.com>
2021-05-10 15:06:50 +0200

    Update setup.py

--- RobocupWrapper/setup.py
+++ RobocupWrapper/setup.py
@@ -1,3 +1,3 @@
   
 from setuptools import setup, find_packages
-setup(name='RobocupWapper', version='1.0', packages=find_packages())
+setup(name='RobocupWapper', version='1.0', language="c++", extra_compile_args=['-std=c++11', '-v'], packages=find_packages())
