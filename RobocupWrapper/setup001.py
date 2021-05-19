465a5abb0d497c4a41be7ef9166bad76a256ed47
evanrex <69971103+evanrex@users.noreply.github.com>
2021-05-19 23:22:50 +0200

    Update setup001.py

--- RobocupWrapper/setup001.py
+++ RobocupWrapper/setup001.py
@@ -6,4 +6,7 @@ module = Extension("RobocupWrapper",sources = ["strategy.cc"])
 setup(name="RobocupWrapper",
     version = "1.0",
     description = "Package description",
+     extra_compile_args=['-std=c++11', '-v'],
+     packages=find_packages(),
     ext_modules =[module])
+
