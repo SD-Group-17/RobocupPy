0a2f975acd9aad0f4441989130c290a9b8d04c12
Evan Rex <evangeorgerex@gmail.com>
2021-05-30 23:02:59 +0200

    Coded simple goal scoring strategy using world data in python

--- Code_Base/behaviors/strategyPy.py
+++ Code_Base/behaviors/strategyPy.py
@@ -6,4 +6,7 @@ def selectSkill(world_data):
     if robocup.playerNumber() == 1:
         return robocup.STAND()
     else:
-        return robocup.HARD_KICK(1.0,3.0)
+        if robocup.gameSide() == 0:
+            return robocup.HARD_KICK(robocup.HALF_FIELD_X +0.1,robocup.HALF_FIELD_Y)
+        else:
+            return robocup.HARD_KICK(- (robocup.HALF_FIELD_X +0.1),robocup.HALF_FIELD_Y)
