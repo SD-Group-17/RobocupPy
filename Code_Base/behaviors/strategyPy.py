2ee2bc13ffe6a4a7ac90fe55419ea3b668fd4d70
Evan Rex <evangeorgerex@gmail.com>
2021-06-01 14:08:43 +0200

    Added functionality to Robocup.py class

--- Code_Base/behaviors/strategyPy.py
+++ Code_Base/behaviors/strategyPy.py
@@ -6,7 +6,13 @@ def selectSkill(world_data):
     if robocup.playerNumber() == 1:
         return robocup.STAND()
     else:
-        if robocup.gameSide() == 0:
-            return robocup.HARD_KICK(robocup.GOAL_LEFT_CENTRE_X +0.1,robocup.GOAL_LEFT_CENTRE_Y)
+        if robocup.teammateDistBall(robocup.playerNumber()) >1.5:
+            return robocup.GO_TO_POSITION(robocup.ballPos()[0],robocup.ballPos()[1])
         else:
-            return robocup.HARD_KICK(robocup.GOAL_RIGHT_CENTRE_X -0.1,robocup.GOAL_RIGHT_CENTRE_Y)
+            if robocup.gameSide() == 0:
+                return robocup.HARD_KICK(robocup.GOAL_LEFT_CENTRE_X +0.1,robocup.GOAL_LEFT_CENTRE_Y)
+            else:
+                return robocup.HARD_KICK(robocup.GOAL_RIGHT_CENTRE_X -0.1,robocup.GOAL_RIGHT_CENTRE_Y)
+        
+
+        
